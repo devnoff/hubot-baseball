fef790540c5d43cb51ba862ebdf74f0ab76412e4
Robinson Park <robin@travelog.me>
2017-12-05 01:21:58 +0900

    Resolve #3 and refactoring

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cache = require('memory-cache')
+const _ = require('lodash')
 
 
 /**
@@ -6,11 +7,17 @@ const cache = require('memory-cache')
  *
  * Created by Robinson Park 2017.11.30. 
  */
-var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */send, botName){
-  var key = 'baseball' + msg.channel
+
+var Baseball = function(args){
+
+  var channel = args.channel
+  var text = args.text
+  var user = args.user_name
+  var send = args.send_handler
+  var bot_name = args.bot_name
+  var key = 'baseball' + channel
   var baseball = cache.get(key)
-  var msg = msg
-  var text = msg.text
+  
 
   var generateNumber = function() {
     var r = []
@@ -27,10 +34,17 @@ var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */s
   }
 
   this.check = function() {
+
+    if (text.indexOf('야구게임 랭킹') > -1) {
+      getRank(function(err, rank){
+        send(rank)
+      });
+      return true
+    }
     
     if (text.indexOf('야구게임 시작') > -1) {
       if (baseball) {
-        send(msg, '게임이 진행 중입니다')
+        send('게임이 진행 중입니다')
       } else {
         this.start()
       }
@@ -45,7 +59,7 @@ var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */s
         // 답변 유효성 검사
         a = a[0]
         if (a[0] == a[1] || a[1] == a[2] || a[0] == a[2]) {
-          send(msg, `<@${msg.user}> 잘못된 번호에요. 같은 번호가 반복되었네요`)
+          send(`<@${user}> 잘못된 번호에요. 같은 번호가 반복되었네요`)
           return true
         }
 
@@ -53,7 +67,7 @@ var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */s
         var answers = baseball.answers
         for (var j in answers) {
           if (answers[j] == a) {
-            send(msg, `<@${msg.user}> ${a}는 이미 답을 한 번호입니다`)
+            send(`<@${user}> ${a}는 이미 답을 한 번호입니다`)
             return true
           }
         }
@@ -74,8 +88,9 @@ var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */s
 
         // 회차 검사
         if (s.length < 3 && times > 7) {
+          this.applyForRank(user, false, times, function(){})
           this.reset()
-          send(msg, `아쉽게도 게임에 지셨네요 ㅠㅠ 답은 ${number} 입니다`) 
+          send(`아쉽게도 게임에 지셨네요 ㅠㅠ 답은 ${number} 입니다`) 
           return true
         }
 
@@ -83,14 +98,15 @@ var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */s
         var m = `제 ${times+1} 회 : \`${a}\` - `
         answers.push(a)
         if (s.length > 2 ) {
+          this.applyForRank(user, true, times, function(){})
           this.reset()
-          send(msg, `정답 [${number}] 축하드립니다~ 승리하셨네요! 🎉🎉🎉`) 
+          send(`정답 [${number}] 축하드립니다~ 승리하셨네요! 🎉🎉🎉`) 
           return true
         } 
 
         // 숫자 판정
         if (s.length < 1 && b.length < 1) {
-          send(msg, m + 'Out~!')
+          send(m + 'Out~!')
         } else {
           var r = []
           if (s.length > 0)
@@ -99,7 +115,7 @@ var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */s
           if (b.length > 0)
             r.push(`\`${b.length}B\``)
 
-          send(msg, m + r.join(' '))
+          send(m + r.join(' '))
         }
         this.saveGame(baseball)
 
@@ -119,12 +135,125 @@ var Baseball = function(/* 봇 메세지 객체 */msg, /* 콜백 (msg, text) */s
     var number = generateNumber()
     var times = 0
     cache.put(key, {number, times, answers})
-    send(msg, `${botName}과 함께 하는 숫자야구⚾️ 게임~! 자 시작합니다~! 🙉`)
+    send(`${bot_name}과 함께 하는 숫자야구⚾️ 게임~! 자 시작합니다~! 🙉`)
   }
 
   this.reset = function() {
     cache.put(key, null)
     cache.del(key)
+  }
+
+  const rank_file_path = __dirname + '/rank.json'
+
+  var readFile = function(callback) {
+    const fs = require('fs')
+
+    fs.readFile(rank_file_path, 'utf8', function(err, data) {
+      if (err) return callback(err, null)
+
+      console.log(data)
+      return callback(null, JSON.parse(data))
+    })
+
+  }
+
+  var writeFile = function(data, callback) {
+    const fs = require('fs')
+    fs.writeFile(rank_file_path, JSON.stringify(data), 'utf8', function(err) {
+      console.log('비동기적 파일 쓰기 완료');
+      callback(err)
+    });
+  }
+
+  this.applyForRank = function(user, isWin, times, callback) {
+    readFile(function(err, data){
+
+      var data = data ? data : []
+
+      var index = _.findIndex(data, function(o) { return o.user == user })
+
+      var user_data = index > -1 ? data[index] 
+                      : { user: user, times: 0, wins: 0}
+
+      user_data.times += times
+      user_data.wins += isWin ? 1 : 0
+
+      if (index > -1)
+        data[index] = user_data
+      else
+        data.push(user_data)
+
+      writeFile(data, function(err){
+        callback(err, data)
+      })
+
+    })
+  }
+
+  var getWinningRateRank = function(callback) {
+    readFile(function(err, data){
+      if (data) {
+          data.sort(function(a, b){
+            var aRate = a.wins / a.times
+            var bRate = b.wins / b.times
+            return aRate == bRate ? 0 : aRate > bRate ? 0 : 1
+          })
+      }
+
+      callback(err, data)
+    })
+  }
+
+  var getWinningsRank = function(callback) {
+    readFile(function(err, data){
+      if (data) {
+        data.sort(function(a, b){
+          return a.wins == b.wins ? 0 : (a.wins > b.wins ? 0 : 1)
+        })
+      }
+
+      callback(err, data)
+    })
+  }
+
+  this.getRank = function(callback) {
+    readFile(function(err, data){
+
+      var result = ""
+      var r = {rate:[], wins:[]}
+      if (data) {
+          var copy = Array.from(data)
+
+          // 정렬
+          data.sort(function(a, b){
+            var aRate = a.wins / a.times
+            var bRate = b.wins / b.times
+            return aRate == bRate ? 0 : aRate > bRate ? 0 : 1
+          })
+
+          // 투구대승률 순위
+          result = "##투구수 대비 승률 순위## \n"
+          for (var i in data) {
+            result = result.concat(`${i+1}등 <@${data[i].user} 승률: ${parseFloat(data[i].wins) / parseFloat(data[i].times)}> \n`)
+          }
+          
+          // 정렬 2
+          copy.sort(function(a, b){
+            return a.wins == b.wins ? 0 : (a.wins > b.wins ? 0 : 1)
+          })
+
+          result = result.concat('\n')
+          result = result.concat('\n')
+          result = result.concat("##최다승 순위## \n")
+          for (var i in copy) {
+            result = result.concat(`${i+1}등 <@${data[i].user} 승리: ${copy[i].wins}> \n`)
+          }
+      } else {
+        result = "데이터가 없습니다"
+      }
+
+      callback(err, result)
+    })
   }
 
 
